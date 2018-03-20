0b1825274a8e4b977f329ee2acc87c2b26f747be
xkatanik <k.katanik@gmail.com>
2018-03-20 09:46:00 +0100

    Added options and methods, .gitignore

--- src/main/java/cz/muni/adap/Main.java
+++ src/main/java/cz/muni/adap/Main.java
@@ -1,6 +1,7 @@
 package cz.muni.adap;
 
 import net.sf.mzmine.datamodel.MZmineProject;
+import net.sf.mzmine.datamodel.RawDataFile;
 import net.sf.mzmine.datamodel.Scan;
 import net.sf.mzmine.datamodel.impl.SimpleMassList;
 import net.sf.mzmine.main.MZmineConfiguration;
@@ -18,114 +19,92 @@ import net.sf.mzmine.parameters.parametertypes.tolerances.MZTolerance;
 import net.sf.mzmine.project.impl.MZmineProjectImpl;
 import net.sf.mzmine.project.impl.ProjectManagerImpl;
 import net.sf.mzmine.project.impl.RawDataFileImpl;
+import org.apache.commons.cli.*;
 
 import java.io.File;
 import java.io.IOException;
 import java.lang.reflect.Field;
 
 /**
+ * ADAP Chromatogram builder module.
+ *
  * @author Kristian Katanik
  */
 public class Main {
 
-    public static void main(String[] args) throws NoSuchFieldException, IllegalAccessException, IOException {
+    public static void main(String[] args) throws IOException, NoSuchFieldException, IllegalAccessException {
 
-        Integer i = 0;
-        String inputFileName = null;
-        String outputFileName = null;
-        Double minScanSpan = null;
-        Double intensityThreshold = null;
-        Double startIntensity = null;
+        String inputFileName;
+        String outputFileName;
+        Double minScanSpan;
+        Double intensityThreshold;
+        Double startIntensity;
         Double mz = 0.001;
         Double ppm = 5.0;
-        while(i < args.length){
-            switch (args[i]){
-                case "-inputFile":
-                    inputFileName = args[i+1];
-                    i += 2;
-                    break;
-                case "-outputFile":
-                    outputFileName = args[i+1];
-                    i += 2;
-                    break;
-                case "-minScanSpan":
-                    try {
-                        minScanSpan = Double.parseDouble(args[i+1]);
-                    } catch (Exception e){
-                        System.err.println("Missing or wrong format of -minScanSpan parameter.");
-                        return;
-                    }
-                    i += 2;
-                    break;
-                case "-intensityThreshold":
-                    try {
-                        intensityThreshold = Double.parseDouble(args[i+1]);
-                    } catch (Exception e){
-                        System.err.println("Missing or wrong format of -intensityThreshold parameter.");
-                        return;
-                    }
-                    i += 2;
-                    break;
-                case "-startIntensity":
-                    try {
-                        startIntensity = Double.parseDouble(args[i+1]);
-                    } catch (Exception e){
-                        System.err.println("Missing or wrong format of -startIntensity parameter.");
-                        return;
-                    }
-                    i += 2;
-                    break;
-                case "-mz":
-                    try {
-                        mz = Double.parseDouble(args[i+1]);
-                    } catch (Exception e){
-                        System.err.println("Missing or wrong format of -mz parameter.");
-                        return;
-                    }
-                    i += 2;
-                    break;
-                case "-ppm":
-                    try {
-                        ppm = Double.parseDouble(args[i+1]);
-                    } catch (Exception e){
-                        System.err.println("Missing or wrong format of -ppm parameter.");
-                        return;
-                    }
-                    i += 2;
-                    break;
-                case "-help":
-                    System.out.println("ADAP Chromatogram builder.\n" +
-                            "This module connects data points from mass lists and builds chromatograms.\n"+
-                            "\n" +
-                            "Required parameters:\n" +
-                            "\t-inputFile | Name or path of input file after Mass detection, ending with .CDF\n" +
-                            "\t-outputFile | Name or path of output file. File name must end with .MPL\n" +
-                            "\t-minScanSpan | Minimum scan span over which some peak in the chromatogram must have (continuous)\n" +
-                            "\t\t points above the noise level to be recognized as a chromatogram.\n" +
-                            "\t\t The optimal value depends on the chromatography system setup.\n" +
-                            "\t\t The best way to set this parameter is by studying the raw data and determining\n" +
-                            "\t\t what is the typical time span of chromatographic peaks.\n" +
-                            "\t-intensityThreshold | This parameter is the intensity value for which intensities greater than\n" +
-                            "\t\t this value can contribute to the minimumScanSpan count.\n" +
-                            "\t-startIntensity | Points below this intensity will not be considered in starting a new chromatogram.\n" +
-                            "\n" +
-                            "Optional parameters:\n" +
-                            "\t-mz and -ppm | Maximum allowed difference between two m/z values to be considered same.\n" +
-                            "\t\t The value is specified both as absolute tolerance (in m/z) and relative tolerance (in ppm).\n" +"" +
-                            "\t\t The tolerance range is calculated using maximum of the absolute and relative tolerances.\n" +
-                            "\t\t[default 0.001 5.0]" +
-                            "\n");
-                    return;
-                default:
-                    i++;
-                    break;
 
-            }
-        }
-        if(intensityThreshold == null || startIntensity == null || minScanSpan == null){
-            System.err.println("Missing required parameters (-minScanSpan, -intensityThreshold or -startIntensity)");
+        Options options = setOptions();
+
+        if(args.length == 0){
+            HelpFormatter helpFormatter = new HelpFormatter();
+            helpFormatter.setOptionComparator(null);
+            helpFormatter.printHelp("ADAP Chromatogram builder module help.", options);
             return;
         }
+
+        CommandLine commandLine;
+        try {
+            commandLine = new DefaultParser().parse(options,args);
+        } catch (ParseException e){
+            for(String arg: args){
+                if(arg.equals("-h") || arg.equals("--help")){
+                    HelpFormatter helpFormatter = new HelpFormatter();
+                    helpFormatter.setOptionComparator(null);
+                    helpFormatter.printHelp("ADAP Chromatogram builder module help.", options);
+                    return;
+                }
+            }
+            System.err.println("Some of the required parameters or their arguments are missing. Use -h or --help for help.");
+            return;
+        }
+
+        inputFileName = commandLine.getOptionValue("i");
+        outputFileName = commandLine.getOptionValue("o");
+        try {
+            minScanSpan = Double.parseDouble(commandLine.getOptionValue("mss"));
+        }catch (NumberFormatException e){
+            System.err.println("Wrong format of minScanSpan value. Value has to be number in double format.");
+            return;
+        }
+        try {
+            intensityThreshold = Double.parseDouble(commandLine.getOptionValue("it"));
+        }catch (NumberFormatException e){
+            System.err.println("Wrong format of intensityThreshold value. Value has to be number in double format.");
+            return;
+        }
+        try {
+            startIntensity = Double.parseDouble(commandLine.getOptionValue("si"));
+        }catch (NumberFormatException e){
+            System.err.println("Wrong format of startIntensity value. Value has to be number in double format.");
+            return;
+        }
+        if(commandLine.hasOption("mz")){
+            try {
+                mz = Double.parseDouble(commandLine.getOptionValue("mz"));
+            } catch (NumberFormatException e) {
+                System.err.println("Wrong format of mz value. Value has to be number in integer format.");
+                return;
+            }
+        }
+        if(commandLine.hasOption("ppm")){
+            try {
+                ppm = Double.parseDouble(commandLine.getOptionValue("ppm"));
+            } catch (NumberFormatException e) {
+                System.err.println("Wrong format of ppm value. Value has to be number in integer format.");
+                return;
+            }
+        }
+
+
 
         File inputFile;
         try {
@@ -168,14 +147,48 @@ public class Main {
         projectManagerField.set(null, projectManager);
         projectManager.setCurrentProject(mZmineProject);
 
-        //Reading and storing MassDetection information from previous step, done by my implementation
+        mZmineProject.addFile(loadMassDetectionData(rawDataFile));
+
+        ADAPChromatogramBuilderParameters parameters = setParameters(minScanSpan, intensityThreshold, startIntensity, mz, ppm);
+
+        ADAPChromatogramBuilderTask adapChromatogramBuilderTask = new ADAPChromatogramBuilderTask(mZmineProject,rawDataFile,parameters);
+        adapChromatogramBuilderTask.run();
+
+        saveData(outputFile);
+    }
+
+    private static Options setOptions(){
+        Options options = new Options();
+        options.addOption(Option.builder("i").required().hasArg().longOpt("inputFile").desc("[required] Name or path of input file. Type .CDF").build());
+        options.addOption(Option.builder("o").required().hasArg().longOpt("outputFile").desc("[required] Name or path of output file. File name must end with .MPL").build());
+        options.addOption(Option.builder("mss").required().hasArg().longOpt("minScanSpan").desc("[required] Minimum scan span over which some peak in the chromatogram must have (continuous)" +
+                " points above the noise level to be recognized as a chromatogram.The optimal value depends on the chromatography system setup." +
+                "The best way to set this parameter is by studying the raw data and determining what is the typical time span of chromatographic peaks.").build());
+        options.addOption(Option.builder("it").required().hasArg().longOpt("intensityThreshold").desc("[required] This parameter is the intensity value for which intensities greater than" +
+                " this value can contribute to the minimumScanSpan count.").build());
+        options.addOption(Option.builder("si").required().hasArg().longOpt("startIntensity").desc("[required] Points below this intensity will not be considered in starting a new chromatogram.").build());
+        options.addOption(Option.builder("mz").required(false).hasArg().desc("Parameter used with parameter -ppm. Maximum allowed difference between two m/z values to be considered same." +
+                " The value is specified both as absolute tolerance (in m/z) and relative tolerance (in ppm)." +
+                "The tolerance range is calculated using maximum of the absolute and relative tolerances. [default 0.001]").build());
+        options.addOption(Option.builder("ppm").required(false).hasArg().desc("Parameter used with parameter -mz. Description via parameter -mz. [default 5.0]").build());
+        options.addOption(Option.builder("h").required(false).longOpt("help").build());
+
+        return options;
+
+    }
+
+    private static RawDataFile loadMassDetectionData(RawDataFileImpl rawDataFile){
+
         int[] scanNumbers = rawDataFile.getScanNumbers();
-        for(int j = 0; j < scanNumbers.length; j++){
-            Scan scan = rawDataFile.getScan(scanNumbers[j]);
-            SimpleMassList massList = new SimpleMassList("masses",scan,scan.getDataPoints());
+        for (int scanNumber : scanNumbers) {
+            Scan scan = rawDataFile.getScan(scanNumber);
+            SimpleMassList massList = new SimpleMassList("masses", scan, scan.getDataPoints());
             scan.addMassList(massList);
         }
-        mZmineProject.addFile(rawDataFile);
+        return rawDataFile;
+    }
+
+    private static ADAPChromatogramBuilderParameters setParameters(Double minScanSpan, Double intensityThreshold, Double startIntensity, Double mz, Double ppm){
 
         ADAPChromatogramBuilderParameters parameters = new ADAPChromatogramBuilderParameters();
         parameters.getParameter(ADAPChromatogramBuilderParameters.scanSelection).setValue(new ScanSelection());
@@ -188,12 +201,11 @@ public class Main {
         parameters.getParameter(ADAPChromatogramBuilderParameters.startIntensity).setValue(startIntensity);
         parameters.getParameter(ADAPChromatogramBuilderParameters.mzTolerance).setValue(new MZTolerance(mz,ppm));
 
+        return parameters;
+    }
 
+    private static void saveData(File outputFile){
 
-        ADAPChromatogramBuilderTask adapChromatogramBuilderTask = new ADAPChromatogramBuilderTask(mZmineProject,rawDataFile,parameters);
-        adapChromatogramBuilderTask.run();
-
-        //Output file tasks
         PeakListsSelection peakListsSelection = new PeakListsSelection();
         peakListsSelection.setSelectionType(PeakListsSelectionType.ALL_PEAKLISTS);
 
@@ -206,4 +218,8 @@ public class Main {
         xmlExportTask.run();
 
     }
+
+
+
+
 }
